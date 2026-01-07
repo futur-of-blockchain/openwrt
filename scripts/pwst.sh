ab076d0f9026a33a28c2e959792f20daef5ed0e7
chrichri <christophe.brun@papit.fr>
2026-01-07 16:08:58 +0100

    FIX installation process in 2 Shell script

--- scripts/pwst.sh
+++ scripts/pwst.sh
@@ -9,19 +9,25 @@ set -ue -o pipefail
 # Patch 2.8.5 to create a Morse M6108 SPI only target.
 # By default it could be SDIO or SPI but SDIO break the Rapsberry
 # buil-in WiFi that is needed.
-patch -p1 < ./patches/rpi4b-sdio-wifi.patch
+git restore target/linux/bcm27xx/image/boards/ekh01/distroconfig.txt
+patch -p1 < ./patches-pwst/rpi4b-sdio-wifi.patch
 
-#######################################################################
+########################################################################
 # Firmware 1.14.1 and corresponding BCF and Smart Manger were tested
 # and found more adapted to the MM610X-H06 – EU than the 1.15.3 ship
 # with Morse Micro OpenWRT 2.8.5
 (
   # To apply in the external Git dependency Morse Micro feeds
   cd ./feeds/morse
-  patch -p1 < ../../patches/morse-feeds.patch
+  git restore kernel/mm-board-config/Makefile
+  git restore kernel/morse-fw/Makefile
+  git restore utils/smart-manager/Makefile
+  patch -p1 < ../../patches-pwst/morse-feeds.patch
 )
 
-#######################################################################
+########################################################################
 # Build configuration sizing the SD card, adding Docker dependencies.
 # Removing unnecessary packages.
+# Restore stored .config first
+git restore .config
 make defconfig

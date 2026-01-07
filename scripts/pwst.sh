07a4163b579669cb69aa21a1613b5081d9e4ca5b
chrichri <christophe.brun@papit.fr>
2026-01-07 11:18:04 +0100

    Switch back Smart Manager, firmware and BCF to version 1.14

--- scripts/pwst.sh
+++ scripts/pwst.sh
@@ -9,8 +9,17 @@ set -ue -o pipefail
 # Patch 2.8.5 to create a Morse M6108 SPI only target.
 # By default it could be SDIO or SPI but SDIO break the Rapsberry
 # buil-in WiFi that is needed.
-
 patch -p1 < ./patches/rpi4b-sdio-wifi.patch
+
+#######################################################################
+# Firmware 1.14.1 and corresponding BCF and Smart Manger were tested
+# and found more adapted to the MM610X-H06 – EU than the 1.15.3 ship
+# with Morse Micro OpenWRT 2.8.5
+(
+  # To apply in the external Git dependency Morse Micro feeds
+  cd ./feeds/morse
+  patch -p1 < ../../patches/morse-feeds.patch
+)
 
 #######################################################################
 # Build configuration sizing the SD card, adding Docker dependencies.
